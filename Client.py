fbe89d20f06263a5308513949dcd52f2c48b75bf
Zachary <zacharypreston1996@protonmail.com>
2021-10-26 14:35:11 -0400

    Client socket created, will attempt to connect, if fails prints error message, otherwise connection established, then close connection

--- Client.py
+++ Client.py
@@ -5,9 +5,19 @@ from datetime import datetime
 #   • a port number on which to listen for incoming connections.
 #   • the name of a log file
 
-def clientProgram():
+def clientProgram(port):
     host = socket.gethostname()
-    print(host)
+    print('Hostname:', host, '\nPort:', port)
+
+    clientSocket = socket.socket()
+    print('Attempting to connect...')
+    try:
+        clientSocket.connect((host, port))
+    except(ConnectionRefusedError):
+        print('--> ERROR: Connection denied')
+    else:
+        print('Connection Established...')
+    clientSocket.close()
 
 if __name__ == '__main__':
-    clientProgram()
+    clientProgram(8000)
