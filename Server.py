673b266e822338582d0fb3d3adc3ec06c421d7a9
Zachary <zacharypreston1996@protonmail.com>
2021-10-26 14:55:44 -0400

    Server file added, listens on the port specified and establishes connection, then closes connection

---
+++ Server.py
@@ -0,0 +1,15 @@
+import socket
+
+def serverProgram(port):
+    host = socket.gethostname()
+    serverSocket = socket.socket()
+    serverSocket.bind((host, port))
+
+    serverSocket.listen(2)
+    connection, address = serverSocket.accept()
+    print('Connection Established: ' + str(address))
+
+    connection.close()
+
+if __name__ == '__main__':
+    serverProgram(8000)
