7df48f20a1f890fb2a73f1145713891012bfcb79
Zachary <zacharypreston1996@protonmail.com>
2021-10-26 18:00:26 -0400

    Added master and slave files to act as a 'Test file' for the client and server files to work across multiple devices

---
+++ Master.py
@@ -0,0 +1,19 @@
+import time
+import socket
+import sys
+import os
+
+s = socket.socket()
+host = socket.gethostname()
+port = 8000
+s.bind(('', port))
+print('Waiting for connections...')
+s.listen()
+conn, addr = s.accept()
+print(addr, 'is connected to server')
+command = input(str('Enter command: '))
+conn.send(command.encode())
+print('command has been sent successfully')
+data = conn.recv(1024)
+if data:
+    print('command received and executed successfully')
